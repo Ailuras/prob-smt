8a9beb38267c21e3c72c020706b39a861c3d7de0
hrcarryu <hanrui22@outlook.com>
2022-11-10 23:39:32 +0800

    test_UD

--- test/run.sh
+++ test/run.sh
@@ -1,5 +1,12 @@
-cd ../build
-make -j20
-cd ../test
-../build/z3 $1 -tr:hr -tr:nlsat -tr:nlsat_smt2 -tr:nlsat_fd -tr:mpq_set
+# cd ../build
+# make -j20
+# cd ../test
+for file in benchmarks/*;do
+    echo --------------------------------------------------
+    start=$[$(date +%s%N)/1000000]
+    ../build/z3 $file
+    end=$[$(date +%s%N)/1000000]
+    take=$(( end - start ))
+    echo $file : ${take} ms.
+done
 # ../build/z3 $1 
